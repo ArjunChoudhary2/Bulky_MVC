e5851a6aaa3095d4017da1175b990b711171b69f
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2024-02-01 12:57:02 +0530

    Method for creating and updating existing category records implemented

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,10 +16,50 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        //creating new category
 
         public IActionResult Create()
         {
             return View(new Category());
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid) 
+            { 
+            _db.Categories.Add(obj); //add category obj to category table using entity framework
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //edit category
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj); //update category obj to category table using entity framework
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
