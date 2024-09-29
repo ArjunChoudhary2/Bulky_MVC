59136912622d97a6e7c05589b03b5b19fff48cd3
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2024-09-29 18:45:00 +0530

    Added Identity Scaffold

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -4,12 +4,15 @@ using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork , UnitOfWork>();
  
 var app = builder.Build();
