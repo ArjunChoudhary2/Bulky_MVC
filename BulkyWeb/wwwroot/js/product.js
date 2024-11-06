e23790239dfd7bce1c2d1ae956b7f31810aef368
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2024-11-06 12:08:17 +0530

    Manage Order datatable implemented.

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -1,13 +1,14 @@
-﻿let DataTable;
+﻿
+let DataTable;
 
 
 $(document).ready(function () {
-   
+
     loadDataTable();
 })
 
 function loadDataTable() {
-   
+
     DataTable = $('#myTable').DataTable({
         "ajax":
         { url:'/admin/product/getall'
@@ -23,9 +24,10 @@ function loadDataTable() {
             {
                 data: 'productId',
                 "render": function (data) {
-                    return `<div class="w-75 btn-group" role="group">
+                    return `< div class="w-75 btn-group" role = "group">
                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit </a>
-                    <a onClick=Delete("/admin/product/delete?id=${data}") class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete </a></div>`
+                    <a onClick=Delete("/admin/product/delete?id=${data}") class="btn btn-danger mx-2" > <i class="bi bi-trash-fill"></i> Delete </a >
+                    </div >`
                 }
                 , "width": '20%'
             }
